9219b0a583e1aefbd4a3355b63d01679c80fbbdf
iuri.barreto <iuribarreto.ib@gmail.com>
2025-03-26 20:32:59 -0300

    teste

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 
 class Jogo:
@@ -23,6 +24,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo="Novo Cadastro")
 
 
@@ -35,5 +38,30 @@ def criar():
     listaJogos.append(jogo)
     return redirect('/')
 
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima = proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        proxima_page = request.form['proxima']
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/{}'.format(proxima_page))
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    if 'usuario_logado' not in session or session ['usuario_logado'] == None:
+        flash('Usuário não logado')
+        return redirect('/')
+    else:
+        session['usuario_logado'] = None
+        flash('usuário deslogado com sucesso!')
+        return redirect('/')
 
 app.run(debug=True)
